0e66c6156655a9f13b6e552df44984b5f0c6b3bb
xiangwenbin <653272838@123>
2026-06-26 10:17:02 +0800

    init

---
+++ app/src/main/java/com/xiang/avlearning/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.xiang.avlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.xiang.avlearning.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'avlearning' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'avlearning' library on application startup.
+        init {
+            System.loadLibrary("avlearning")
+        }
+    }
+}
